4d69b1b4b19f0bcd3c64e6c1ef99e8d00f2b89b7
MarkTyrkba <awfulmarco@gmail.com>
2024-04-06 01:02:05 +0300

    Added optional dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,11 @@ extern crate proc_macro;
 extern crate drylib_proc_macros;
 #[cfg(feature = "decls")]
 mod drylib_declarative_macros;
+
+/* TODO:
+    update README
+    update examples
+    update tests
+    clean code
+    do better macros
+*/
