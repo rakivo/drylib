fc376a0efe92ac811ec23cb1dfa6e9123c7146f8
MarkTyrkba <awfulmarco@gmail.com>
2024-04-06 01:54:40 +0300

    make crate drylib_proc_macros pub so you can use it from drylib

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
-extern crate proc_macro;
-extern crate drylib_proc_macros;
+pub extern crate drylib_proc_macros;
 #[cfg(feature = "decls")]
-mod drylib_declarative_macros;
+mod drylib_decl_macros;
 
 /* TODO:
     update README
     update examples
     update tests
+    create proper documentation
     clean code
     do better macros
 */
